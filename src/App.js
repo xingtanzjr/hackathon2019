19f71ff7716d38aa1a8d3a9cde1480e53b27afde
Jinrui.Zhang <jinrzhan@microsoft.com>
2019-07-19 17:09:56 +0800

    change ppt

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Hackathon Slide Ads</p>
-        <iframe src="https://microsoftapc-my.sharepoint.com/personal/jinrzhan_microsoft_com/_layouts/15/Doc.aspx?sourcedoc={ab3891b5-5d3c-4c26-a6c2-9a4e4f0a575a}&amp;action=embedview&amp;wdAr=1.3333333333333333" width="350px" height="286px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
+        <iframe src="https://microsoftapc-my.sharepoint.com/personal/jinrzhan_microsoft_com/_layouts/15/Doc.aspx?sourcedoc={3f962154-2898-44d1-b343-96037bed7ef9}&amp;action=embedview&amp;wdAr=1.3333333333333333&amp;wdEaa=1" width="350px" height="286px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
       </header>
     </div>
   );
