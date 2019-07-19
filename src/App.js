b867332dfe09d08ac42913cf678b2118e6867d89
Jinrui.Zhang <jinrzhan@microsoft.com>
2019-07-19 15:56:45 +0800

    init env

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hackathon Slide Ads</p>
+        <iframe src="https://microsoftapc-my.sharepoint.com/personal/jinrzhan_microsoft_com/_layouts/15/Doc.aspx?sourcedoc={ab3891b5-5d3c-4c26-a6c2-9a4e4f0a575a}&amp;action=embedview&amp;wdAr=1.3333333333333333" width="350px" height="286px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
       </header>
     </div>
   );
