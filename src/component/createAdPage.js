42523a3e1661536ee34b18a79277022bb408511c
Jinrui.Zhang <jinrzhan@microsoft.com>
2019-07-22 18:33:47 +0800

    add sidebar

--- src/component/createAdPage.js
+++ src/component/createAdPage.js
@@ -4,6 +4,156 @@ import { Input, Icon, Upload, message, Button } from 'antd';
 
 const { Search } = Input;
 
+class SidebarItem extends React.Component {
+    render() {
+        const icon = this.props.icon ? (
+            <div className='icon'>
+                <Icon type={this.props.icon} style={{ fontSize: '20px' }} />
+            </div>) : null;
+        return (
+            <div className={`side-bar-item${this.props.active ? ' active': ''}`}>
+                {icon}
+                <div className='text'>
+                    <span>{this.props.text}</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Sidebar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.campaignNameList = [];
+        for (let i = 0 ; i < 10 ; i ++ ){
+            this.campaignNameList.push(`Campaign #${i}`);
+        }
+        this.campaignList = [];
+        this.campaignNameList.forEach((name, idx) => {
+            this.campaignList.push(
+                <SidebarItem key={`cp-${idx}`} icon={'pause'} text={name} />
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='side-bar'>
+                    <SidebarItem icon={'appstore'} text={'All Campaigns'} />
+                    <div className='spliter-bar' />
+                    <SidebarItem icon={'monitor'} text={'Search Campaigns'} />
+                    <SidebarItem active icon={'shop'} text={'Shopping Campaigns'} />
+                    <SidebarItem icon={'thunderbolt'} text={'Dynamic Search Ads'} />
+                    <SidebarItem icon={'switcher'} text={'Audience Campaign'} />
+                    <div className='spliter-bar' />
+                    <SidebarItem active icon={'play-circle'} text={'Slide Show - 1'} />
+                    {this.campaignList}
+                </div>
+                <div className='side-bar adgroup'>
+                    <SidebarItem icon={'appstore'} text={'AdGroups'} />
+                    <div className='spliter-bar' />
+                    <SidebarItem text={'Overview'} />
+                    <SidebarItem text={'Recommendations'} />
+                    <SidebarItem active text={'Ads & extensions'} />
+                    <SidebarItem text={'Landing pages'} />
+                    <SidebarItem text={'Keywords'} />
+                    <SidebarItem text={'Settings'} />
+                    <SidebarItem text={'Demographics'} />
+                    <SidebarItem text={'Locations'} />
+                    <SidebarItem text={'Ad schedule'} />
+                </div>
+            </div>
+        );
+    }
+}
+
+function topBar() {
+    return (
+        <div className='top-bar'>
+            <img src={microsoftLogo} alt='ms' />
+            <div className='spliter' />
+            <span className='logo-span'>Advertising</span>
+            <Search
+                onSearch={console.log('haha')}
+            />
+            <div className='function-control'>
+                <Icon type="arrow-up" style={{ fontSize: '20px' }} />
+                <span>Import</span>
+            </div>
+            <div className='function-control'>
+                <Icon type="line-chart" style={{ fontSize: '20px' }} />
+                <span>Reports</span>
+            </div>
+            <div className='function-control'>
+                <Icon type="tool" style={{ fontSize: '20px' }} />
+                <span>Tools</span>
+            </div>
+            <div className='spliter' />
+            <div className='function-control'>
+                <Icon type="question" style={{ fontSize: '20px' }} />
+                <span>Help</span>
+            </div>
+            <div className='function-control'>
+                <Icon type="bell" style={{ fontSize: '20px' }} />
+                <span>Notif</span>
+            </div>
+        </div>
+    );
+}
+
+class MainContent extends React.Component {
+    render() {
+        return (
+            <div className='main'>
+                <span className='title'>Ads</span>
+                <div className='spliter-bar' />
+                <span style={{ 'fontSize': '18px', 'fontWeight': 'bold' }}> Create a Slide Show Ad</span>
+                <div className='content'>
+                    <div className='input-line'>
+                        <span className='label'>Ad Title:</span>
+                        <Input placeholder='input ad name' />
+                    </div>
+                    <div className='input-line'>
+                        <span className='label'>Destination URL:</span>
+                        <Input placeholder='input URL' />
+                    </div>
+                    <div className='input-line'>
+                        <span className='label'>Keywords:</span>
+                        <Input placeholder='input description' />
+                    </div>
+                    <div className='input-line'>
+                        <span className='label'>Description:</span>
+                        <Input placeholder='input description' />
+                    </div>
+                    <div className='input-line'>
+                        <span className='label'>PPT:</span>
+                        <Upload {...this.props.uploadProps}>
+                            <Button>
+                                <Icon type="upload" /> Click to Upload
+                                </Button>
+                        </Upload>
+                    </div>
+                    <div className='button-group'>
+                        <Button type="primary">Submit</Button>
+                        <Button >Cancel</Button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Preview extends React.Component {
+    render() {
+        return (
+            <div className='preview'>
+            </div>
+        );
+    }
+}
+
 class CreateAdPage extends React.Component {
 
     constructor(props) {
@@ -30,64 +180,10 @@ class CreateAdPage extends React.Component {
     render() {
         return (
             <div className='create-ad-container'>
-                <div className='top-bar'>
-                    <img src={microsoftLogo} alt='ms' />
-                    <div className='spliter' />
-                    <span className='logo-span'>Advertising</span>
-                    <Search
-                        onSearch={console.log('haha')}
-                    />
-                    <div className='function-control'>
-                        <Icon type="arrow-up" style={{ fontSize: '20px' }} />
-                        <span>Import</span>
-                    </div>
-                    <div className='function-control'>
-                        <Icon type="line-chart" style={{ fontSize: '20px' }} />
-                        <span>Reports</span>
-                    </div>
-                    <div className='function-control'>
-                        <Icon type="tool" style={{ fontSize: '20px' }} />
-                        <span>Tools</span>
-                    </div>
-                    <div className='spliter' />
-                    <div className='function-control'>
-                        <Icon type="question" style={{ fontSize: '20px' }} />
-                        <span>Help</span>
-                    </div>
-                    <div className='function-control'>
-                        <Icon type="alert" style={{ fontSize: '20px' }} />
-                        <span>Notif</span>
-                    </div>
-                </div>
+                {topBar()}
                 <div className='create-ad-body'>
-                    <span className='title'>Create Slide show Ad</span>
-                    <div className='spliter-bar' />
-                    <div className='content'>
-                        <div className='input-line'>
-                            <span className='label'>Name:</span>
-                            <Input placeholder='input ad name' style={{ width: '400px' }} />
-                        </div>
-                        <div className='input-line'>
-                            <span className='label'>Destination URL:</span>
-                            <Input placeholder='input URL' style={{ width: '400px' }} />
-                        </div>
-                        <div className='input-line'>
-                            <span className='label'>Description:</span>
-                            <Input placeholder='input description' style={{ width: '400px' }} />
-                        </div>
-                        <div className='input-line'>
-                            <span className='label'>PPT:</span>
-                            <Upload {...this.uploadProps}>
-                                <Button>
-                                    <Icon type="upload" /> Click to Upload
-                                </Button>
-                            </Upload>
-                        </div>
-                        <div className='button-group'>
-                            <Button type="primary">Submit</Button>
-                            <Button >Cancel</Button>
-                        </div>
-                    </div>
+                    <Sidebar />
+                    <MainContent uploadProps={this.uploadProps}/>
                 </div>
             </div>
         );
